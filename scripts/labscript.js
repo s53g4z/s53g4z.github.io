e8f8c5fd70466728e1e1d9ddf3ec226c23637fe3
nobody <nobody@example.com>
2020-12-26 16:18:53 -0800

    Rewrite lab

--- scripts/labscript.js
+++ scripts/labscript.js
@@ -1,122 +1,152 @@
 //
 
 let step = 5;
+let border = 1;  // width of one border = 1px, and have two borders to consider
 let keyWdown = false;
 let keyDdown = false;
 let keyAdown = false;
 let boxArr = new Array();
 
-function onSurface(box) {
-	let styles = getComputedStyle(box.handle);
-	let onGround = Math.ceil(parseFloat(styles.top) + parseFloat(styles.height) +
-		parseFloat(box.vy)) >= window.innerHeight;
-	if (onGround)
+function F(item) {
+	return parseFloat(item);
+}
+
+function onOrCloseToGround(box) {
+	let boxBottom = F(box.styl.top) + F(box.styl.height) + border;
+	if (window.innerHeight > boxBottom && window.innerHeight - boxBottom < step ||
+		boxBottom >= window.innerHeight)
 		return true;
+	return false;
+}
+
+// Smash the box into the ground.
+function smashIntoGround(box) {
+	box.hndl.style.top = (window.innerHeight - border - F(box.styl.height)) +
+		"px";
+}
+
+function canCollideVertically(box1, box2, direction) {
+	let top1 = F(box1.styl.top) - border;
+	let bottom1 = top1 + border + F(box1.styl.height) + border;
+	let top2 = F(box2.styl.top) - border;
+	let bottom2 = top2 + border + F(box2.styl.height) + border;
+
+	if (direction == "down") {
+		top1 += box1.vy;
+		bottom1 += box1.vy;
+	}
+
+	return inBetween(top2, top1, bottom1) ||
+		inBetween(top1, top2, bottom2) && inBetween(bottom1, top2, bottom2) ||
+		inBetween(bottom2, top1, bottom1);
+}
+
+function canCollideHorizontally(box1, box2, direction) {
+	let left1 = F(box1.styl.left) - border;
+	let right1 = F(box1.styl.left) + F(box1.styl.width) + border;
+	let left2 = F(box2.styl.left) - border;
+	let right2 = F(box2.styl.left) + F(box2.styl.width) + border;
+	
+	
+	
+	return (inBetween(left2, left1, right1) ||
+		inBetween(left1, left2, right2) && inBetween(right1, left2, right2) ||
+		inBetween(right2, left1, right1));
+}
+
+// Make box fall.
+function fall(box) {
+	if (onOrCloseToGround(box) && box.vy > 0) {
+		smashIntoGround(box);
+		return;
+	}
+	for (let box2 of boxArr) {
+		if (box == box2)
+			continue;
+		let bottom1 = F(box.styl.top) + F(box.styl.height) + border;
+		let top2 = F(box2.styl.top) - border;
+		if (canCollideVertically(box, box2, "down") &&
+			canCollideHorizontally(box, box2, "down") && 
+			Math.abs(bottom1 - top2) < step)
+			return;
+	}
+	box.vy += box.ay;
+	if (F(box.styl.top) + F(box.styl.height) + border + box.vy >
+		window.innerHeight) {
+		box.vy = 0;
+		smashIntoGround(box);
+	} else {
+		box.hndl.style.top = (F(box.styl.top) + box.vy) + "px";
+	}
+	
+	return;
+}
+
+function canTouch(box1, box2) {
+	return true;
+}
+
+// Is line between top and bottom?
+function inBetween(line, top, bottom) {
+	if (top > bottom)
+		throw new Error("top must be less than bottom for inBetween()");
+	return top < line && line < bottom;
+}
+
+function touching(box1, box2, direction) {
+	let left1 = F(box1.styl.left) - border + step;
+	let right1 = left1 + F(box1.styl.width) + border;
+	let top1 = F(box1.styl.top) - border;
+	let left2 = F(box2.styl.left) - border;
+	let right2 = left2 + F(box2.styl.width) + border;
+	let top2 = F(box2.styl.top) - border;
+	
+	if (!canTouch(box1, box2))
+		return false;
+	if (direction == "right" && canCollideVertically(box1, box2) &&
+		canCollideHorizontally(box1, box2)) {
+		return right1 > left2;
+	}
+	return false;
+}
+
+function move(direction, box) {
+	if (onOrCloseToGround(box) && direction == "down")
+		return;
 	for (let i = 0; i < boxArr.length; i++) {
 		if (box == boxArr[i])
 			continue;
-		let styles1 = box.st;
-		let bot1 = Math.ceil(parseFloat(styles1.top) + parseFloat(styles1.height));
-		let styles2 = boxArr[i].st;
-		let top2 = Math.ceil(parseFloat(styles2.top));
-		if (canCollideHorizontally(styles1, styles2, "down") &&
-			canCollideVertically(styles1, styles2, "down") &&
-			bot1 + parseFloat(box.vy) >= top2)
-				return true;
+		if (touching(box, boxArr[i], direction))
+			return;
 	}
-	return false;;
+	if (direction == "right")
+		box.hndl.style.left = (F(box.styl.left) + step) + "px";
+	if (direction == "left")
+		box.hndl.style.left = (F(box.styl.left) - step) + "px";
 }
 
-function fall(box) {
-	let styles = getComputedStyle(box.handle);
-	box.vy += box.ay;
-	box.style.top = Math.ceil(parseFloat(styles.top) + box.vy) + "px";
-}
-
-function jump(box) {
-	let styles = getComputedStyle(box.handle);
-	if (onSurface(box))
-		box.vy = -5;
-}
-
-// Return if a is between x and y.
-function isBetween(a, x, y) {
-	if (x > y)
-		throw new Error("x must be <= y");
-	return x < a && a < y;
-}
-
-function canCollideHorizontally(styles1, styles2, direction) {
-	let top1 = Math.ceil(parseFloat(styles1.top));
-	let bot1 = top1 + Math.ceil(parseFloat(styles1.height));
-	let top2 = Math.ceil(parseFloat(styles2.top));
-	let bot2 = top2 + Math.ceil(parseFloat(styles2.height));
-	
-	if (direction == "down") {
-		top1 += step;  // bad approximation, should use vy instead
-		bot1 += step;  // ibid
-	} else if (direction == "up") {
-		top1 -= step;
-		bot1 -= step;
+function jump(box1) {
+	if (onOrCloseToGround(box1)) {
+		box1.vy = -5;
+		return;
 	}
-	
-	let ret = isBetween(top2, top1, bot1) ||
-		isBetween(top1, top2, bot2) && isBetween(bot1, top2, bot2) ||
-		isBetween(bot2, top1, bot1);
-	
-	return ret;
-}
-
-function canCollideVertically(styles1, styles2, direction) {
-	let left1 = Math.ceil(parseFloat(styles1.left));
-	let right1 = left1 + Math.ceil(parseFloat(styles1.width));
-	let left2 = Math.ceil(parseFloat(styles2.left));
-	let right2 = left2 + Math.ceil(parseFloat(styles2.width));
-	
-	if (direction == "left") {
-		left1 -= step;
-		right1 -= step;
-	} else if (direction == "right") {
-		left1 += step;
-		right1 += step;
-	}
-	
-	let ret = isBetween(left2, left1, right1) ||
-		isBetween(left1, left2, right2) && isBetween(right1, left2, right2) ||
-		isBetween(right2, left1, right1);
-		
-	return ret;
-}
-
-function handleLeft(styles1, styles2, item1) {
-	let left1 = Math.ceil(parseFloat(styles1.left));
-	let right2 = Math.ceil(parseFloat(styles2.left) +
-		parseFloat(styles2.width));
-	let canHitHoriz = canCollideHorizontally(styles1, styles2);
-	let canHitVert = canCollideVertically(styles1, styles2, "left");
-	let canHit = canHitHoriz && canHitVert;
-	if (!canHit ||
-		canHit && left1 - step > right2)
-		item1.style.left = (left1 - step) + "px";
-}
-
-function move (direction, item1) {
-	for (let item2 of boxArr) {
-		if (item1 == item2)
+	for (let box2 of boxArr) {
+		if (box1 == box2)
 			continue;
-		let styles1 = getComputedStyle(item1.handle);
-		let styles2 = getComputedStyle(item2.handle);
-		if (direction == "left") {
-			handleLeft(styles1, styles2, item1);
-		} else
-			throw new Error("unknown direction to move()");
+		box1Bottom = F(box1.styl.top) + F(box1.styl.height) + border;
+		box2Top = F(box2.styl.top) - border;
+		if (canCollideVertically(box1, box2) &&
+			canCollideHorizontally(box1, box2) &&
+			box1Bottom - box2Top < step) {
+				box1.vy = -5;
+				return;
+		}
 	}
 }
 
 function loop() {
-	let styles = getComputedStyle(boxArr[0].handle);
 	if (keyDdown) {
-		boxArr[0].style.left = (parseFloat(styles.left) + step) + "px";
+		move("right", boxArr[0]);
 	} else if (keyAdown) {
 		move("left", boxArr[0]);
 	}
@@ -124,8 +154,7 @@ function loop() {
 		jump(boxArr[0]);
 	}
 	for (let box of boxArr) {
-		if (!onSurface(box))
-			fall(box);
+		fall(box)
 	}
 	requestAnimationFrame(loop);
 }
@@ -150,26 +179,24 @@ function initializeInput() {
 	};
 }
 
-function Box(handle, vx, vy, ax, ay) {
-	this.handle = handle;
-	this.style = handle.style;
-	this.st = getComputedStyle(handle);
-	this.vx = vx;
-	this.vy = vy;
-	this.ax = ax;
+function Box(hndl, ay) {
+	this.hndl = hndl;
+	this.styl = getComputedStyle(hndl);
+	this.vx = this.vy = this.ax = 0;
 	this.ay = ay;
 }
 
-// Initialize the boxes.
-function initializeBoxes() {
-	let box1 = new Box(document.getElementById("box1"), 0, 0, 0, 0.2);
-	boxArr.push(box1);
-	let box2 = new Box(document.getElementById("box2"), 0, 0, 0, 0);
-	boxArr.push(box2);
+function initBoxes() {
+	let a = new Box(document.querySelector("#box1"), 0.2);
+	let b = new Box(document.querySelector("#box2"), 0.0);
+	let c = new Box(document.querySelector("#box3"), 0.0);
+	boxArr.push(a);
+	boxArr.push(b);
+	boxArr.push(c);
 }
 
 (function() {
 	initializeInput();
-	initializeBoxes();
+	initBoxes();
 	loop();
 })();
